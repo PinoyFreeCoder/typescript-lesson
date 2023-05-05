ef09b119ea22641cb95462a4a93bf7719a3ec20b
PinoyFreeCoder <pinoyfreecoder@gmail.com>
2023-05-06 00:18:21 +0800

    union_types and custom types

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
-enum Access {ADMIN = '123', MODERATOR = 345, USER = 111}
+type PinoyFreeCoder = number | string;
 
-const student: {
-    name: string;
-    age : number;
-    gender?: string
-    interest : string[]
-    access : Access
-} = {
-    name : 'Juan',
-    age :  19,
-    interest : ["Basketball", "Reading"],
-    access : Access.ADMIN
+function addOrCombine(data1 : PinoyFreeCoder, data2 : PinoyFreeCoder){
+   let result : PinoyFreeCoder;
+   if (typeof data1 === 'number' && typeof data2 === 'number'){
+    result = data1 + data2;
+   }else{
+    result = data1.toString() + data2.toString()
+   }
+   return result;
 }
 
-if(student.access === '123'){
-    console.log('is admin')
-}
+
+const combineNumbers = addOrCombine(3, 4);
+console.log(combineNumbers)
+
+const combineString = addOrCombine("Pinoy", "FreeCoder")
+console.log(combineString)
